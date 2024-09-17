f32d3f356cab9c941085cf015bcbf7f143f90a05
donimhmmdf <donim.fajar25@gmail.com>
2024-09-17 14:16:01 +0700

    Change refactor methode sumDouble

--- LogicPhase/Exercises/Day01/SumDouble.java
+++ LogicPhase/Exercises/Day01/SumDouble.java
@@ -2,10 +2,7 @@ package LogicPhase.Exercises.Day01;
 
 public class SumDouble {
     public static int sumDouble(int a, int b) {
-        if (a == b) {
-            return (a + b) * 2;
-        }
-        return a + b;
+        return a == b ? ((a + b) * 2) : (a + b);
     }
 
     public static void main(String[] args) {
