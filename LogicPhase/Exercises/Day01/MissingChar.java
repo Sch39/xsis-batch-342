2e7b5449712c415023211352ade5caedc661d377
donimhmmdf <donim.fajar25@gmail.com>
2024-09-17 16:02:51 +0700

    Feat - add settlement for MissingChar problem with passed test cases

---
+++ LogicPhase/Exercises/Day01/MissingChar.java
@@ -0,0 +1,13 @@
+package Exercises.Day01;
+
+public class MissingChar {
+    private static String missingChar(String str, int n) {
+        return new StringBuilder(str).deleteCharAt(n).toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(missingChar("kitten", 1));
+        System.out.println(missingChar("kitten", 0));
+        System.out.println(missingChar("kitten", 4));
+    }
+}
