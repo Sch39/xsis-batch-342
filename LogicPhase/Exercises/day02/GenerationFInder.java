6ef9e5d130bd8c430c133c1792d831996afed870
Sch39 <sukronraezpactoer@gmail.com>
2024-09-18 09:42:42 +0700

    Feat - add settlement for GenerationFinder problem with passed tests

---
+++ LogicPhase/Exercises/day02/GenerationFInder.java
@@ -0,0 +1,27 @@
+package logicphase.exercises.day02;
+
+public class GenerationFInder {
+  public static String getName(Integer year) {
+    String name = "";
+
+    if (year <= 1945) {
+      name = "The Builders";
+    } else if (year <= 1946) {
+      name = "Baby Boomers";
+    } else if (year <= 1965) {
+      name = "Generation X";
+    } else if (year <= 1980) {
+      name = "Generation Y";
+    } else if (year <= 1995) {
+      name = "Generation Z";
+    } else if (year <= 2010) {
+      name = "Generation Alpha";
+    } else if (year <= 2025) {
+      name = "Generation Beta";
+    } else {
+      name = "Not defined";
+    }
+
+    return name;
+  }
+}
