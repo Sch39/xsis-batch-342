80ac26fe563d9aafe73baecf4d8212f88aedd71c
Sch39 <sukronraezpactoer@gmail.com>
2024-09-18 10:40:14 +0700

    Feat - add settlement and test for InRange30And50 problem

---
+++ LogicPhase/Exercises/day02/InRange30And50.java
@@ -0,0 +1,10 @@
+package logicphase.exercises.day02;
+
+public class InRange30And50 {
+  public static Boolean isInRange(float num1, float num2) {
+    return (num1 >= 30
+        && num1 <= 50
+        && num2 >= 30
+        && num2 <= 50);
+  }
+}
