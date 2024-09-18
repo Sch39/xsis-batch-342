6285be5f13141adb0411357f289163747286cf5a
Sch39 <sukronraezpactoer@gmail.com>
2024-09-18 08:25:50 +0700

    Feat - add unit test for diff21 problem with scenario when number les than and greater than 21

--- LogicPhase/Exercises/Day01/__tests__/Diff21Test.java
+++ LogicPhase/Exercises/Day01/__tests__/Diff21Test.java
@@ -9,11 +9,15 @@ import logicphase.exercises.day01.Diff21;
 public class Diff21Test {
   @Test
   public void it_should_return_diff_when_less_than_21() {
-    assertEquals(19, Diff21.diff21(2));
-    assertEquals(20, Diff21.diff21(1));
-    assertEquals(21, Diff21.diff21(0));
+    assertEquals(2, Diff21.diff21(19));
+    assertEquals(1, Diff21.diff21(20));
+    assertEquals(0, Diff21.diff21(21));
   }
 
-  // @Test
-  // public
+  @Test
+  public void it_should_return_result_of_diff_multiply_by_2_when_greater_than_21() {
+    assertEquals(2, Diff21.diff21(22));
+    assertEquals(4, Diff21.diff21(23));
+    assertEquals(6, Diff21.diff21(24));
+  }
 }
