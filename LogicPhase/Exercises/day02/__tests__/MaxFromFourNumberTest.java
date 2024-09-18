f0adcb370a2785f2a9e67fac3cb2667023d01438
Sch39 <sukronraezpactoer@gmail.com>
2024-09-18 15:46:31 +0700

    Change refactor test name in MaxFromFourNumberTest

--- LogicPhase/Exercises/day02/__tests__/MaxFromFourNumberTest.java
+++ LogicPhase/Exercises/day02/__tests__/MaxFromFourNumberTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 import logicphase.exercises.day02.MaxFromFourNumber;
 
 public class MaxFromFourNumberTest {
-  public void test_getMinMax_withNormalValues() {
+  @Test
+  public void it_should_return_min_max_with_normies_values() {
     int[] numbers = { 3, 5, 7, 2 };
     String expected = "min: 2\nmax: 7";
     String actual = MaxFromFourNumber.getMinMax(numbers);
@@ -15,7 +16,7 @@ public class MaxFromFourNumberTest {
   }
 
   @Test
-  public void test_getMinMax_withNegativeValues() {
+  public void it_should_return_min_max_with_negative_values() {
     int[] numbers = { -10, -5, 0, 5 };
     String expected = "min: -10\nmax: 5";
     String actual = MaxFromFourNumber.getMinMax(numbers);
@@ -23,7 +24,7 @@ public class MaxFromFourNumberTest {
   }
 
   @Test
-  public void test_getMinMax_withAllEqualValues() {
+  public void it_should_return_min_max_with_all_equal_values() {
     int[] numbers = { 4, 4, 4, 4 };
     String expected = "min: 4\nmax: 4";
     String actual = MaxFromFourNumber.getMinMax(numbers);
@@ -31,7 +32,7 @@ public class MaxFromFourNumberTest {
   }
 
   @Test
-  public void test_getMinMax_withSingleValue() {
+  public void it_should_return_min_max_with_single_value() {
     int[] numbers = { 42 };
     String expected = "min: 42\nmax: 42";
     String actual = MaxFromFourNumber.getMinMax(numbers);
