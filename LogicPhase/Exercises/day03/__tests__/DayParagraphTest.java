1252f9dbd5631377342896d8b15a663ad25555df
Sch39 <sukronraezpactoer@gmail.com>
2024-09-19 11:44:08 +0700

    Add test cases for problem DayParagraph

--- LogicPhase/Exercises/day03/__tests__/DayParagraphTest.java
+++ LogicPhase/Exercises/day03/__tests__/DayParagraphTest.java
@@ -20,6 +20,16 @@ public class DayParagraphTest {
 
     assertArrayEquals(expected1, DayParagraph.getPara("senin"));
 
+    String[] expected7 = {
+        "kemarin lusa adalah hari minggu",
+        "kemarin adalah hari senin",
+        "hari ini adalah selasa",
+        "besok adalah hari rabu",
+        "besok lusa adalah hari kamis",
+    };
+
+    assertArrayEquals(expected7, DayParagraph.getPara("selasa"));
+
     String[] expected2 = {
         "kemarin lusa adalah hari senin",
         "kemarin adalah hari selasa",
@@ -28,6 +38,42 @@ public class DayParagraphTest {
         "besok lusa adalah hari jum'at",
     };
     assertArrayEquals(expected2, DayParagraph.getPara("rabu"));
+
+    String[] expected3 = {
+        "kemarin lusa adalah hari selasa",
+        "kemarin adalah hari rabu",
+        "hari ini adalah kamis",
+        "besok adalah hari jum'at",
+        "besok lusa adalah hari sabtu",
+    };
+    assertArrayEquals(expected3, DayParagraph.getPara("kamis"));
+
+    String[] expected4 = {
+        "kemarin lusa adalah hari rabu",
+        "kemarin adalah hari kamis",
+        "hari ini adalah jum'at",
+        "besok adalah hari sabtu",
+        "besok lusa adalah hari minggu",
+    };
+    assertArrayEquals(expected4, DayParagraph.getPara("jum'at"));
+
+    String[] expected5 = {
+        "kemarin lusa adalah hari kamis",
+        "kemarin adalah hari jum'at",
+        "hari ini adalah sabtu",
+        "besok adalah hari minggu",
+        "besok lusa adalah hari senin",
+    };
+    assertArrayEquals(expected5, DayParagraph.getPara("sabtu"));
+
+    String[] expected6 = {
+        "kemarin lusa adalah hari jum'at",
+        "kemarin adalah hari sabtu",
+        "hari ini adalah minggu",
+        "besok adalah hari senin",
+        "besok lusa adalah hari selasa",
+    };
+    assertArrayEquals(expected6, DayParagraph.getPara("minggu"));
   }
 
   @Test
