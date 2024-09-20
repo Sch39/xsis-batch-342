da2b8ff5cb008b38f0ad1fbe18f5b4da32f7c692
Sch39 <sukronraezpactoer@gmail.com>
2024-09-20 09:20:28 +0700

    Change delete commented code

--- LogicPhase/Exercises/day04/MatrixIndexPattern.java
+++ LogicPhase/Exercises/day04/MatrixIndexPattern.java
@@ -4,19 +4,6 @@ import java.util.Scanner;
 
 public class MatrixIndexPattern {
   public static String getPattern(int n) {
-    // String pattern = "";
-    // for (int i = 0; i < n; i++) {
-    // for (int j = 0; j < n; j++) {
-    // pattern += String.valueOf(i) + String.valueOf(j);
-    // if (j < n - 1) {
-    // pattern += " ";
-    // }
-    // }
-
-    // if (i < n - 1) {
-    // pattern += "\n";
-    // }
-    // }
     return getPattern(n, n);
   }
 
