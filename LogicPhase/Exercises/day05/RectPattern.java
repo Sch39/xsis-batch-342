4ae837fa48aaa40a50b1f6ee172a82fac8fd88e9
Sch39 <sukronraezpactoer@gmail.com>
2024-09-23 09:16:18 +0700

    Feat add solution for rect pattern and add unit test

---
+++ LogicPhase/Exercises/day05/RectPattern.java
@@ -0,0 +1,25 @@
+package logicphase.exercises.day05;
+
+public class RectPattern {
+  public static String[][] getPattern(int size) {
+    String[][] pattern = new String[size][size];
+
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if (i == 0 || i == size - 1) {
+          pattern[i][j] = "*";
+        } else if (j == 0 || j == size - 1) {
+          pattern[i][j] = "*";
+        } else {
+          pattern[i][j] = " ";
+        }
+      }
+    }
+
+    return pattern;
+  }
+
+  public static void main(String[] args) {
+    getPattern(5);
+  }
+}
