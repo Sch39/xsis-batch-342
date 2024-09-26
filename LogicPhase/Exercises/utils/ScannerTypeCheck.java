f1a138b86b99146b99151c695a117b7c58aff5c2
Sch39 <sukronraezpactoer@gmail.com>
2024-09-26 14:03:56 +0700

    Add jdoc

--- LogicPhase/Exercises/utils/ScannerTypeCheck.java
+++ LogicPhase/Exercises/utils/ScannerTypeCheck.java
@@ -3,13 +3,43 @@ package logicphase.exercises.utils;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 
+/**
+ * The {@code ScannerTypeCheck} class provides a utility for validating user
+ * input using a {@link Scanner}.
+ * It repeatedly prompts the user for input until a valid input is provided, as
+ * determined by the provided
+ * callback function.
+ * 
+ * @author sch39
+ */
 public class ScannerTypeCheck {
   private Scanner scanner;
 
+  /**
+   * Constructs a new {@code ScannerTypeCheck} instance with the specified
+   * {@link Scanner}.
+   *
+   * @param scanner the {@code Scanner} to be used for reading user input
+   */
   public ScannerTypeCheck(Scanner scanner) {
     this.scanner = scanner;
   }
 
+  /**
+   * Checks the input by executing the given {@code callback}. If an exception
+   * occurs during the execution of
+   * the callback, an error message is printed, and the user is prompted to enter
+   * input again.
+   *
+   * @param <T>                  the type of the result produced by the
+   *                             {@code callback}
+   * @param callback             a {@link Callable} that represents the logic for
+   *                             checking input
+   * @param customInvalidMessage a custom message to display when the input is
+   *                             invalid
+   * @return the valid input result produced by the {@code callback}
+   * @throws Exception if the callback throws an exception that is not handled
+   */
   public <T> T checkInput(Callable<T> callback, String customInvalidMessage) {
     while (true) {
       try {
