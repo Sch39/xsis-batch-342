2ab9686279c707de88da4da0d0859e93e3aa45cd
Sch39 <sukronraezpactoer@gmail.com>
2024-09-30 10:36:17 +0700

    Fix holiday input in main

--- src/main/java/dev/sch39/bootcamp/logicphase/day09/packagedelivery/Main.java
+++ src/main/java/dev/sch39/bootcamp/logicphase/day09/packagedelivery/Main.java
@@ -1,5 +1,7 @@
 package dev.sch39.bootcamp.logicphase.day09.packagedelivery;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -18,30 +20,60 @@ public class Main {
     ScannerTypeCheck scannerTypeCheck = new ScannerTypeCheck(scanner);
 
     while (true) {
-      int dayInWeek = scannerTypeCheck.checkInput(
-          () -> scanner.nextInt(),
-          "Masukkan hari dalam minggu (1: Senin, 2: Selasa, ..., 7: Minggu):",
-          "Input tidak valid. Silakan masukkan angka 1-7.");
+      int dayInWeek;
+      while (true) {
+        dayInWeek = scannerTypeCheck.checkInput(
+            () -> scanner.nextInt(),
+            "Masukkan hari dalam minggu (1: Senin, 2: Selasa, ..., 7: Minggu):",
+            "Input harus berupa angka. Silakan masukkan angka 1-7.");
+        if (dayInWeek >= 1
+            && dayInWeek <= 7) {
+          break;
+        } else {
+          System.out.println("Input tidak valid. Silakan masukkan angka 1-7.");
+        }
+      }
       order.orderedInWeek(dayInWeek);
 
-      int dayInMonth = scannerTypeCheck.checkInput(
-          () -> scanner.nextInt(),
-          "Masukkan hari dalam bulan (1-31):",
-          "Input tidak valid. Silakan masukkan angka 1-31.");
+      int dayInMonth;
+      while (true) {
+        dayInMonth = scannerTypeCheck.checkInput(
+            () -> scanner.nextInt(),
+            "Masukkan hari dalam bulan (1-31):",
+            "Input harus berupa angka. Silakan masukkan angka 1-31.");
+        if (dayInMonth >= 1
+            && dayInMonth <= 31) {
+          break;
+        } else {
+          System.out.println("Input tidak valid. Silakan masukkan angka 1-31.");
+        }
+      }
       order.orderedInMonth(dayInMonth);
-
+      List<Integer> holidaysArr = new ArrayList<>();
       System.out.println("Masukkan hari libur nasional (masukkan -1 jika selesai):");
       while (true) {
-        int holiday = scannerTypeCheck.checkInput(
-            () -> scanner.nextInt(),
-            "Masukkan hari libur nasional (1-31):",
-            "Input tidak valid. Silakan masukkan angka 1-31.");
+        int holiday;
+        while (true) {
+          holiday = scannerTypeCheck.checkInput(
+              () -> scanner.nextInt(),
+              "Masukkan hari libur nasional (0-31):",
+              "Input harus berupa angka. Silakan masukkan angka 0-31.");
+          if ((holiday >= 0
+              && holiday <= 31)
+              || holiday == -1) {
+            break;
+          } else {
+            System.out.println("Input tidak valid. Silakan masukkan angka 0-31.");
+          }
+        }
+
+        holidaysArr.add(holiday);
 
         if (holiday == -1) {
           break;
         }
-        order.setNationalHoliday(holiday);
       }
+      order.setNationalHoliday(holidaysArr.toArray(new Integer[0]));
 
       Map<String, Integer> estimatedDelivery = order.estimate2Destination();
       String estimatedDay = order.getNameDayInWeek(estimatedDelivery.get("dayInWeek"));
@@ -55,8 +87,8 @@ public class Main {
       System.out.println("Apakah Anda ingin melakukan estimasi lain? (y/n):");
       String answer = scanner.next().toLowerCase();
 
-      if (!answer.equals("ya")) {
-        System.out.println("Terima kasih telah menggunakan layanan estimasi pengiriman.");
+      if (!answer.equals("y")) {
+        System.out.println("Than u.");
         break;
       }
     }
