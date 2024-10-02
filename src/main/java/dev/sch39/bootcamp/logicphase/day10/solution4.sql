23727a42f576c6b783e2e44827c30124069a2466
Sch39 <sukronraezpactoer@gmail.com>
2024-10-02 09:43:18 +0700

    Add distinc query for solution4

--- src/main/java/dev/sch39/bootcamp/logicphase/day10/solution4.sql
+++ src/main/java/dev/sch39/bootcamp/logicphase/day10/solution4.sql
@@ -1,6 +1,15 @@
-SELECT st.name,
+SELECT st.code,
+  st.name,
   sc.score,
   ex.is_active
+FROM students st
+  INNER JOIN scores sc ON st.code = sc.student_code
+  INNER JOIN exams ex ON sc.exam_code = ex.code
+WHERE sc.score > 80
+  AND ex.is_active = TRUE;
+-- with distinc
+SELECT DISTINCT st.code,
+  st.name
 FROM students st
   INNER JOIN scores sc ON st.code = sc.student_code
   INNER JOIN exams ex ON sc.exam_code = ex.code
