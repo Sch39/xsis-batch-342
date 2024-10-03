f3b0d616a6f4a41d9daf76e3f51f769231745b7a
Sch39 <sukronraezpactoer@gmail.com>
2024-10-03 11:34:50 +0700

    Fix avg salary

--- src/main/java/dev/sch39/bootcamp/logicphase/day11/solutions/11.sql
+++ src/main/java/dev/sch39/bootcamp/logicphase/day11/solutions/11.sql
@@ -1,9 +1,11 @@
 SELECT CONCAT(bi.first_name, ' ', bi.last_name) fullname,
   em.nip,
   em.status,
-  AVG(em.salary * 12) avg_salary
+  em.salary,
+  AVG(em.salary) avg_salary
 FROM employees em
   JOIN biodata bi ON em.biodata_id = bi.id
 GROUP BY fullname,
   em.nip,
+  em.salary,
   em.status;
