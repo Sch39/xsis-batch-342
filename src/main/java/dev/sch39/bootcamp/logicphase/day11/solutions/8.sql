8058da1eb58cfefec29870f892867acd96aaa3c7
Sch39 <sukronraezpactoer@gmail.com>
2024-10-03 09:19:15 +0700

    Fix  duration(array) to duration day

--- src/main/java/dev/sch39/bootcamp/logicphase/day11/solutions/8.sql
+++ src/main/java/dev/sch39/bootcamp/logicphase/day11/solutions/8.sql
@@ -4,7 +4,7 @@ SELECT lr.id leave_request_id,
   bi.last_name,
   le.type leave_type,
   lr.reason,
-  AGE(lr.end_date, lr.start_date) duration,
+  DATE_PART('day', AGE(lr.end_date, lr.start_date)) duration_day,
   cp.contact
 FROM leave_requests lr
   RIGHT JOIN employees em ON lr.employee_id = em.id
